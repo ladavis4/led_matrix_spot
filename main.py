b2ff4a2af0d82c27ed4281380cf34364301b6f6c
ladavis4 <ladavisiv@gmail.com>
2022-07-08 22:33:57 -0400

    ready to test on pi

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import json
 import time
 
 # Settings
-SIM = False
+SIM = True
 # Globals
 global temp, weather_image, stock_prices, spotify_flag, spotify_image  # Information updated from callbacks
 
