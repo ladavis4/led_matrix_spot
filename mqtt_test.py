bd353e63be923ccaf2e220d3f00eb59384117406
ladavis4 <ladavisiv@gmail.com>
2024-09-23 20:37:23 +0900

    Added a bunch of new topics

--- mqtt_test.py
+++ mqtt_test.py
@@ -2,28 +2,71 @@ import json
 
 import paho.mqtt.client as mqtt
 # The callback for when the client receives a CONNACK response from the server.
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    # This function is the general MQTT callback. If the message is unidentified it
+    # will handle the data
+    print("An unidentified message was detected")
+    print(msg.payload.decode("utf-8"))
+
+def power_callback(client, userdata, msg):
+    # Handles power toggle for the LED Screen
+    print("A power message was received")
+    print(msg.payload.decode("utf-8"))
+
+def bright_callback(client, userdata, msg):
+    # Handles brightness settings for the LED Screen
+    print("A brightness message was received")
+    print(msg.payload.decode("utf-8"))
+
+def picture_callback(client, userdata, msg):
+    # Handles brightness settings for the LED Screen
+    print("A picture message was received")
+    print(msg.payload.decode("utf-8"))
+
+def weather_callback(client, userdata, msg):
+    # Handles brightness settings for the LED Screen
+    print("A weather message was received")
+    print(msg.payload.decode("utf-8"))
+
+def calendar_callback(client, userdata, msg):
+    # Handles brightness settings for the LED Screen
+    print("A calendar message was received")
+    print(msg.payload.decode("utf-8"))
+
+def spotify_callback(client, userdata, msg):
+    # Handles brightness settings for the LED Screen
+    print("A spotify message was received")
+    print(msg.payload.decode("utf-8"))
+
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("oki/lr/ac/mode/set")
+    client.subscribe("oki/screen/#")
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.payload)
+    client.publish("homeassistant/switch/ledscreen_power/config", power_json_obj)
+    client.publish("homeassistant/number/ledscreen/config", bright_json_obj)
+    client.publish("homeassistant/switch/ledscreen_picture_sw/config", picture_json_obj)
+    client.publish("homeassistant/switch/ledscreen_weather_sw/config", weather_json_obj)
+    client.publish("homeassistant/switch/ledscreen_calednar_sw/config", calendar_json_obj)
+    client.publish("homeassistant/switch/ledscreen_spotify_sw/config", spotify_json_obj)
 
+    client.message_callback_add("oki/screen/power/set", power_callback)
+    client.message_callback_add("oki/screen/bright/set", bright_callback)
+    client.message_callback_add("oki/screen/pic/set", picture_callback)
+    client.message_callback_add("oki/screen/cal/set", calendar_callback)
+    client.message_callback_add("oki/screen/weather/set", weather_callback)
+    client.message_callback_add("oki/screen/spot/set", spotify_callback)
 
 # Define constants
 MQTT_SERVER = "192.168.0.95"
 MQTT_PORT = 1883
 DEVICE_ID = "ledScreen"
 
-# Define MQTT Topics
-TOPIC_STATE = "oki/lr/screen/"
-TOPIC_COMMAND = "oki/lr/screen/set"
-
 #Discovery JSON
-dict1 = {
+power_json = {
     "name" : "power",
     "unique_id": "ledscreen_power",
     "command_topic": "oki/screen/power/set",
@@ -36,7 +79,7 @@ dict1 = {
     }
 }
 
-dict2 = {
+bright_json = {
     "name" : "brightness",
     "unique_id": "ledscreen_brightness",
     "command_topic": "oki/screen/bright/set",
@@ -47,19 +90,58 @@ dict2 = {
     }
 }
 
-json_object_1 = json.dumps(dict1)
-json_object_2 = json.dumps(dict2)
+picture_json = {
+    "name" : "pictures_sw",
+    "unique_id": "ledscreen_picture_sw",
+    "command_topic": "oki/screen/pic/set",
+    "device" : {
+        "name" : "RPi LED Device",
+        "identifiers": "ledscreen"
+    }
+}
 
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
+weather_json = {
+    "name" : "weather_sw",
+    "unique_id": "ledscreen_weather_sw",
+    "command_topic": "oki/screen/weather/set",
+    "device" : {
+        "name" : "RPi LED Device",
+        "identifiers": "ledscreen"
+    }
+}
 
-mqttc.connect("192.168.0.95", 1883, 60)
-mqttc.publish("homeassistant/switch/ledscreen/config", json_object_1)
-mqttc.publish("homeassistant/number/ledscreen/config", json_object_2)
-mqttc.subscribe("oki/screen/bright/set")
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-mqttc.loop_forever()
+calendar_json = {
+    "name" : "calendar_sw",
+    "unique_id": "ledscreen_calendar_sw",
+    "command_topic": "oki/screen/cal/set",
+    "device" : {
+        "name" : "RPi LED Device",
+        "identifiers": "ledscreen"
+    }
+}
+
+spotify_json = {
+    "name" : "spotify_sw",
+    "unique_id": "ledscreen_spotify_sw",
+    "command_topic": "oki/screen/spot/set",
+    "device" : {
+        "name" : "RPi LED Device",
+        "identifiers": "ledscreen"
+    }
+}
+
+power_json_obj = json.dumps(power_json)
+bright_json_obj = json.dumps(bright_json)
+picture_json_obj = json.dumps(picture_json)
+weather_json_obj = json.dumps(weather_json)
+calendar_json_obj = json.dumps(calendar_json)
+spotify_json_obj = json.dumps(spotify_json)
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("192.168.0.95", 1883, 60)
+
+
+client.loop_forever()
